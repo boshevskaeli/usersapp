98bedadd37f3e129f7ffe44a17025a7a211d1218
Zane Mojsovski <zhankom@vestelglobal.com>
2021-10-25 16:35:42 +0200

    redux persist test

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import AppNavigator from './app/navigation';
 import ListScreen from './app/screens/ListScreen/index';
-import {store} from 'app/store/store';
+import {reduxstore} from 'app/store/store';
+import {persistor} from 'app/store/store';
 const App = () => (
-  <Provider store={store}>
-    {/* <AppNavigator /> */}
-    <ListScreen/>
+  <Provider store={reduxstore}>
+    <PersistGate loading={null} persistor={persistor}>
+      {/* <AppNavigator /> */}
+      <ListScreen />
+    </PersistGate>
   </Provider>
 );
 
--- app/store/store.js
+++ app/store/store.js
@@ -1,8 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './reducers/userSlice';
+import {configureStore} from '@reduxjs/toolkit';
+import {store} from './rootReducer';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-  reducer: {
-      user: userReducer,
-  },
-})
+// export const store = configureStore({
+//   reducer: {
+//     user: userReducer,
+//   },
+// });
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, store);
+const reduxstore = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
